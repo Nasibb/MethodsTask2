1c8fffaf92c8843b26d285aa0e03076fef52213a
Nasibb <nasibgi@code.edu.az>
2023-11-01 14:57:23 +0400

    Start

---
+++ Methods2/Program.cs
@@ -0,0 +1,59 @@
+﻿#region Task1
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//int TwoNum (int n, int m)
+//{
+//    int sum = default;
+//    for (int i=n; i<=m; i++)
+//    {
+//        if (i%2 == 1)
+//        { sum += i; };
+//    }
+//    return sum;
+//}
+//Console.WriteLine(TwoNum (1,100));
+
+#endregion
+
+#region Task2
+//Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//int SumNumbers(int[] numbers)
+//{
+//    foreach (int i in numbers)
+//    {
+
+//    }
+//    return 0;
+//}
+
+
+//int[] oddNum = { 29, 2, 30, 123 };
+//int sum = 0;
+//foreach (int num in oddNum)
+//{
+//    if (num%2 == 1)
+//    {
+//        sum += num;
+//    }
+//}
+//Console.WriteLine(sum);
+
+#endregion
+
+#region Task3
+
+#endregion
+
+#region Task4
+//Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
+
+//int Num (int n)
+//{
+//    for (int i=0; i<=n; i++)
+//    {
+
+//    }
+//}
+
+#endregion
