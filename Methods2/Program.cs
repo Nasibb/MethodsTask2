c88779bca40ad94f06863dba66cf6288bedd0ad6
Nasibb <nasibgi@code.edu.az>
2023-11-02 15:41:11 +0400

    -Return

--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -1,5 +1,5 @@
 ﻿#region Task1
-//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.+
 
 //int TwoNum (int n, int m)
 //{
@@ -16,44 +16,110 @@
 #endregion
 
 #region Task2
-//Verilmish arrayin icindeki tek ededlerin cemini tapin.
+//Verilmish arrayin icindeki tek ededlerin cemini tapin.+
 
-//int SumNumbers(int[] numbers)
+//int SumOddNumbers(params int[] numbers)
 //{
+//    int sum = default;
 //    foreach (int i in numbers)
 //    {
-
+//        if (i%2 != 0)
+//        {
+//            sum += i;
+//        }    
 //    }
-//    return 0;
+//    return sum;
 //}
-
-
-//int[] oddNum = { 29, 2, 30, 123 };
-//int sum = 0;
-//foreach (int num in oddNum)
-//{
-//    if (num%2 == 1)
-//    {
-//        sum += num;
-//    }
-//}
-//Console.WriteLine(sum);
+//Console.WriteLine(SumOddNumbers (1,2,3,4,5,6,7,8,9,10));
 
 #endregion
 
 #region Task3
+//Verilmish arrayin icindeki cut ededlerin sayini tapin.+
+
+//int CountEvenNumbers (int[] numbers)
+//{
+//    int count = 0;
+//    foreach (int n in numbers)
+//        if (n %2== 0)
+//        {
+//            count++;
+//        }
+//    return count;
+//}
+//int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12 };
+//Console.WriteLine(CountEvenNumbers(nums));
 
 #endregion
 
 #region Task4
 //Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
 
-//int Num (int n)
+//int FindEvenOddNumers (int n)
 //{
-//    for (int i=0; i<=n; i++)
-//    {
-
-//    }
+//    while (n==0)
 //}
 
+//int num = 123;
+//int division = default;
+//for (int i = 1; i<=num; i++)
+//{
+//    if (i % 2 == 0)
+
+
+//}
+//Console.WriteLine(division);
+
+
 #endregion
+
+#region Task5
+
+//n ededi gelir. 2-ni hansisa quvvete yukseltdikde n-in alinan cavaba beraber olub-olmamasını tapın.
+//Meselen: Cavablar ola biler: 2,4,8,16,32 ve s. Yeni n bele ededler olmalidir.
+
+
+#endregion
+
+#region Task6
+//Verilmish arrayin-in elementlerinin icinden 1-20 arasinda olanlarinin hasilini tapmaq.Mes ([1,3,55,6,33], cavab : 18).+RETURN
+
+using System.Diagnostics.CodeAnalysis;
+
+//int MultiNumbers(params int[] numbers)
+//{
+//    int multi = 1;
+//    foreach (int number in numbers)
+//        if (number >= 1 && number <= 20)
+//        {
+//            multi *= number;
+//        }
+//    return multi;
+//}
+////Console.WriteLine(MultiNumbers(21, 23, 123, 35));
+//Console.WriteLine(MultiNumbers(21,23,5,15,18,123,35,20,1,10,5));
+
+#endregion
+
+#region Task7
+//Verilmish arrayin elementlerininden cut olanlarinin ceminin kvadratini tapmaq.+RETURN
+
+//int Calculate(params int[] numbers)
+//{
+//    int sum = 0;
+//    int exponentiation = 0;
+//    foreach (var number in numbers)
+//    {
+//        if (number % 2 == 0)
+//        { 
+//            sum += number;
+//            exponentiation = sum * sum;
+//        }
+//    }
+//    return exponentiation;
+//}
+//Console.WriteLine(Calculate(1,2,3,4,5,6,7,8,9,10));
+
+#endregion
+
+
